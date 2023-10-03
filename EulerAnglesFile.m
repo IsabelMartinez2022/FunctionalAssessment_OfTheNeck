10973c01ca5e761690ac363f00d20476f26d4d52
IsabelMartinez2022 <101266828+IsabelMartinez2022@users.noreply.github.com>
2023-10-03 22:50:04 +0200

    Create extracted files
    Creates files with the extracted data from the headset

---
+++ EulerAnglesFile.m
@@ -0,0 +1,31 @@
+%Saves the Euler angles measured with the headset to a new file
+
+rootfolder = pwd;
+patientsA= dir(fullfile(rootfolder,'GroupA','*.csv'));
+
+for i = 1:length(patientsA)
+    pathA = fullfile(rootfolder,'GroupA');
+    patient_data = readtable(fullfile(pathA, patientsA(i).name));
+    % We search for the headset (H) data
+    headset_data = patient_data(strcmp(patient_data.Var3, 'H'), :);
+        % We extract the Euler angles (x, y, z) from the headset 
+        euler_angles = headset_data(:, 11:13);
+        % Save the extracted data to a new CSV file
+        writetable(euler_angles, strcat(erase(patientsA(i).name,".csv"),'_extracted.csv'));
+end
+
+rootfolder = pwd;
+patientsB= dir(fullfile(rootfolder,'GroupB','*.csv'));
+
+for i = 1:length(patientsB)
+    pathB = fullfile(rootfolder,'GroupB');
+    patient_data = readtable(fullfile(pathB, patientsB(i).name));
+    % We search for the headset (H) data
+    headset_data = patient_data(strcmp(patient_data.Var3, 'H'), :);
+        % We extract the Euler angles (x, y, z) from the headset 
+        euler_angles = headset_data(:, 11:13);
+        % Save the extracted data to a new CSV file
+        writetable(euler_angles,strcat(erase(patientsB(i).name,".csv"),'_extracted.csv'));
+end
+
+
