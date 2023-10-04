211d715a9e57e58bf5c7aa065ab360b6b66a58eb
IsabelMartinez2022 <101266828+IsabelMartinez2022@users.noreply.github.com>
2023-10-04 13:09:58 +0200

    Solved NaN errors
    Also, added Group files to the repository

--- EulerAnglesFile.m
+++ EulerAnglesFile.m
@@ -6,7 +6,9 @@ patientsA= dir(fullfile(rootfolder,'GroupA','*.csv'));
 
 for i = 1:length(patientsA)
     pathA = fullfile(rootfolder,'GroupA');
-    patient_data = readtable(fullfile(pathA, patientsA(i).name));
+    opts = detectImportOptions(fullfile(pathA, patientsA(i).name));
+    opts = setvartype(opts,'string');
+    patient_data = readtable(fullfile(pathA, patientsA(i).name),opts);
     % We search for the headset (H) data
     headset_data = patient_data(strcmp(patient_data.Var3, 'H'), :);
         % We extract the Euler angles (x, y, z) from the headset 
@@ -18,15 +20,17 @@ end
 rootfolder = pwd;
 patientsB= dir(fullfile(rootfolder,'GroupB','*.csv'));
 
-for i = 1:length(patientsB)
+for j = 1:length(patientsB)
     pathB = fullfile(rootfolder,'GroupB');
-    patient_data = readtable(fullfile(pathB, patientsB(i).name));
+    opts = detectImportOptions(fullfile(pathB, patientsB(j).name));
+    opts = setvartype(opts,'string');
+    patient_data = readtable(fullfile(pathB, patientsB(j).name),opts);
     % We search for the headset (H) data
     headset_data = patient_data(strcmp(patient_data.Var3, 'H'), :);
         % We extract the Euler angles (x, y, z) from the headset 
         euler_angles = headset_data(:, 11:13);
         % We save the extracted data to a new CSV file
-        writetable(euler_angles,strcat(erase(patientsB(i).name,".csv"),'_extracted.csv'));
+        writetable(euler_angles,strcat(erase(patientsB(j).name,".csv"),'_extracted.csv'));
 end
 
 
