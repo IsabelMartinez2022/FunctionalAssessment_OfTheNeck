c75268b5d9685268ec27c4b33a66942f01921624
IsabelMartinez2022 <101266828+IsabelMartinez2022@users.noreply.github.com>
2023-10-31 00:54:57 +0100

    New folders and files created

--- EulerAnglesFile.m
+++ EulerAnglesFile.m
@@ -4,6 +4,10 @@
 rootfolder = pwd;
 patientsA= dir(fullfile(rootfolder,'GroupA','*.csv'));
 
+if not(isfolder("GroupA_extracted"))
+mkdir("GroupA_extracted")
+end
+
 for i = 1:length(patientsA)
     pathA = fullfile(rootfolder,'GroupA');
     opts = detectImportOptions(fullfile(pathA, patientsA(i).name));
@@ -14,11 +18,17 @@ for i = 1:length(patientsA)
         % We extract the Euler angles (x, y, z) from the headset 
         euler_angles = headset_data(:, 11:13);
         % We save the extracted data to a new CSV file
-        writetable(euler_angles, strcat(erase(patientsA(i).name,".csv"),'_extracted.csv'));
+        cd (rootfolder+"\GroupA_extracted\");
+        filename= strcat(erase(patientsA(i).name,".csv"),'_extracted.csv');
+        writetable(euler_angles, filename);
 end
 
-rootfolder = pwd;
+cd (rootfolder);
 patientsB= dir(fullfile(rootfolder,'GroupB','*.csv'));
+
+if not(isfolder("GroupB_extracted"))
+mkdir("GroupB_extracted")
+end
 
 for j = 1:length(patientsB)
     pathB = fullfile(rootfolder,'GroupB');
@@ -30,6 +40,8 @@ for j = 1:length(patientsB)
         % We extract the Euler angles (x, y, z) from the headset 
         euler_angles = headset_data(:, 11:13);
         % We save the extracted data to a new CSV file
+        cd (rootfolder+"\GroupB_extracted\");
+        filename= strcat(erase(patientsA(i).name,".csv"),'_extracted.csv');
         writetable(euler_angles,strcat(erase(patientsB(j).name,".csv"),'_extracted.csv'));
 end
 
--- Spectrogram.m
+++ Spectrogram.m
@@ -1,7 +1,15 @@
 %Task 2
 %Calculation of the spectrogram of each patient and saved as a png image
 
-for i = 1:200
+rootfolder= pwd;
+groupAfolder= rootfolder+"\GroupA_extracted\";
+cd (groupAfolder);
+
+if not(isfolder("GroupA_spectrograms"))
+mkdir("GroupA_spectrograms")
+end
+
+for i=1:200
     if isfile("Pte_"+i+"​_extracted.csv")
         read_data= readtable("Pte_"+i+"​_extracted.csv");
         read_data_array = table2array(read_data);
@@ -33,8 +41,48 @@ for i = 1:200
         subplot(3,1,3)
         waterplot(s_z,f_z,t_z);
 
-        filename= "Spectrogram_Patient"+i+".png";
-        saveas(gcf,filename);
+        cd (groupAfolder+"\GroupA_spectrograms\");
+        saveas(gcf,"Spectrogram_Patient"+i+".png");
+        cd (groupAfolder);
+    end 
+end
+
+groupBfolder= rootfolder+"\GroupB_extracted\";
+cd (groupBfolder);
+
+if not(isfolder("GroupB_spectrograms"))
+mkdir("GroupB_spectrograms")
+end
+
+for j = 1:200
+    if isfile("Pte_"+j+"​_extracted.csv")
+        read_data= readtable("Pte_"+j+"​_extracted.csv");
+        read_data_array = table2array(read_data);
+        %patient_xyz contains the Euler angles
+        patient_xyz= str2double(strrep(read_data_array,',','.')); 
+
+        fs= 50; %sampling rate of Unity's fixedUpdate()
+        window_length = 256;
+        overlap = window_length/2;
+        nfft = 512;
+      
+        %Calculation of the spectrogram for each time series
+        [s_x,f_x,t_x]=spectrogram(patient_xyz(:,1),window_length,overlap,nfft,fs);
+        figure;
+        subplot(3,1,1)
+        waterplot(s_x,f_x,t_x);
+
+        [s_y,f_y,t_y]=spectrogram(patient_xyz(:,2),window_length,overlap,nfft,fs);
+        subplot(3,1,2)
+        waterplot(s_y,f_y,t_y);
+        
+        [s_z,f_z,t_z]=spectrogram(patient_xyz(:,3),window_length,overlap,nfft,fs);
+        subplot(3,1,3)
+        waterplot(s_z,f_z,t_z);
+
+        cd (groupBfolder+"\GroupB_spectrograms\");
+        saveas(gcf,"Spectrogram_Patient"+j+".png");
+        cd (groupBfolder);
     end 
 end
 
