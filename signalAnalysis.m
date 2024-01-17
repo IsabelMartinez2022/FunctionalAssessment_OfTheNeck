e7bfb888a3aa1fc13f6e7542e5e5e7a74ad24e9d
IsabelMartinez2022 <101266828+IsabelMartinez2022@users.noreply.github.com>
2024-01-17 17:35:40 +0100

    new changes

--- signalAnalysis.m
+++ signalAnalysis.m
@@ -1,14 +1,14 @@
 %Tasks 4, 5 and 6
-%Average spectral power and duration 
+%Average spectral power and duration of the spectrograms
 %Statistical tests
-%Average duration of the spectrograms with its standard deviation 
-
 
 average_groupA= spectralPower('GroupA');
 average_groupB= spectralPower('GroupB');
-[hypothesis_values, p_values,means,std_deviations]= analysis(average_groupA, average_groupB);
-[avg_duration_groupA, stdev_duration_groupA]= durationSpectrogram('GroupA');
-[avg_duration_groupB, stdev_duration_groupB]= durationSpectrogram('GroupB');
+[hypothesis_values_freq, p_values_freq,means,std_deviations]= analysis_frequency(average_groupA, average_groupB);
+
+duration_groupA= durationSpectrogram('GroupA');
+duration_groupB= durationSpectrogram('GroupB');
+[hypothesis_value_duration, p_value_duration]= analysis_duration(duration_groupA,duration_groupB);
 
 
 function avgpower= spectralPower (groupname)
@@ -47,12 +47,20 @@ for i=1:200
         patient_y = patient_y(discard_samples+1:end);
         patient_z=patient_xyz(:,6)-patient_xyz(1,6);
         patient_z = patient_z(discard_samples+1:end);
+        %Sum of all three time series
+        patient_xyz=abs(patient_x)+abs(patient_y)+abs(patient_z);
         
         %Power spectral densities are calculated using the spectrogram function for each band
-        [~,~,~,psx]= spectrogram(patient_x,window_length,overlap,f,fs,"power",'yaxis');
-        [~,~,~,psy]= spectrogram(patient_y,window_length,overlap,f,fs,"power",'yaxis');
-        [~,~,~,psz]= spectrogram(patient_z,window_length,overlap,f,fs,"power",'yaxis');
-        [~,~,~,psxyz]= spectrogram((abs(patient_x)+abs(patient_y)+abs(patient_z)),window_length,overlap,f,fs,'yaxis');
+        %Time series are centred
+        patient_x_centred=patient_x-mean(patient_x);
+        patient_y_centred=patient_y-mean(patient_y);
+        patient_z_centred=patient_z-mean(patient_z);
+        patient_xyz_centred=patient_xyz-mean(patient_xyz);
+
+        [~,~,~,psx]= spectrogram(patient_x_centred,window_length,overlap,f,fs,"power",'yaxis');
+        [~,~,~,psy]= spectrogram(patient_y_centred,window_length,overlap,f,fs,"power",'yaxis');
+        [~,~,~,psz]= spectrogram(patient_z_centred,window_length,overlap,f,fs,"power",'yaxis');
+        [~,~,~,psxyz]= spectrogram(patient_xyz_centred,window_length,overlap,f,fs,'yaxis');
         
         %Average
         avgpower(patient_index,:,1)= mean(psx,2);
@@ -68,12 +76,12 @@ cd (rootfolder)
 end 
 
 
-function [hypothesis_values,p_values,means,std_deviations]= analysis (avg_groupA, avg_groupB)
+function [hypothesis_values_freq,p_values_freq,means,std_deviations]= analysis_frequency(avg_groupA, avg_groupB)
 
 h_groupA= zeros(4,11);
 h_groupB= zeros(4,11);
-hypothesis_values= zeros(4,11);
-p_values= zeros(4,11);
+hypothesis_values_freq= zeros(4,11);
+p_values_freq= zeros(4,11);
 means=zeros(2,11,4);
 std_deviations=zeros(2,11,4);
 
@@ -102,14 +110,14 @@ for k = 1:4
 
         %We perform t-test or Whitney-U-test based on normality assumptions
         if h_groupA(k,j)|| h_groupB(k,j)
-            [p_values(k,j),hypothesis_values(k,j)]= ranksum(avg_groupA(:,j,k), avg_groupB(:,j,k));
+            [p_values_freq(k,j),hypothesis_values_freq(k,j)]= ranksum(avg_groupA(:,j,k), avg_groupB(:,j,k));
         else 
-            [hypothesis_values(k,j),p_values(k,j)]= ttest2(avg_groupA(:,j,k), avg_groupB(:,j,k));
+            [hypothesis_values_freq(k,j),p_values_freq(k,j)]= ttest2(avg_groupA(:,j,k), avg_groupB(:,j,k));
         end 
         
         %We calculate mean and std deviation for those bands with
         %statistical differences between groups
-        if hypothesis_values(k,j)
+        if hypothesis_values_freq(k,j)
             means(1,j,k)= mean(avg_groupA(:,j,k));
             means(2,j,k)= mean(avg_groupB(:,j,k));
             std_deviations(1,j,k)= std(avg_groupA(:,j,k));
@@ -121,25 +129,18 @@ end
 end
 
 
-%We calculate the average duration of the spectrograms and its std deviation 
-%for each group
-function [avg_duration, stdev_duration]= durationSpectrogram(groupname)
+%Duration of the spectrograms 
+function duration_patients= durationSpectrogram(groupname)
 
 rootfolder = pwd;
 addpath(rootfolder);
 groupfolder= rootfolder+"\"+groupname+"_extracted\";
 cd (groupfolder);
 
-avg_duration= zeros(2,1);
-stdev_duration= zeros (2,1);
 duration_patients= zeros(1,30);
 index=1;
 
 for i=1:200
-
-    window_length =128;
-    overlap = 18;
-    f=[1, 3, 7, 9, 11, 13, 15, 17, 19, 21, 23];
 
     if isfile("Pte_"+i+"​_extracted.csv")
 
@@ -154,8 +155,22 @@ for i=1:200
     end 
 end
 
-avg_duration= mean(duration_patients);
-stdev_duration= std(duration_patients);
-
 cd (rootfolder)
 end 
+
+
+function [hypothesis_value_duration, p_value_duration]= analysis_duration(duration_groupA,duration_groupB)
+
+%To check normality
+h_groupA = adtest(duration_groupA);
+h_groupB = adtest(duration_groupB);
+
+%We perform t-test or Whitney-U-test based on normality assumptions
+if h_groupA|| h_groupB
+    [p_value_duration,hypothesis_value_duration]= ranksum(duration_groupA,duration_groupB);
+else 
+    [hypothesis_value_duration,p_value_duration]= ttest2(duration_groupA,duration_groupB);
+end 
+       
+end
+   
